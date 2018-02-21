4230ff99d3b575cc40609ca0bd064f09d3e29427
GB <ultraturbomotive@gmail.com>
2018-02-21 10:11:02 +0900

    commit

--- TeamProject/src/kr/co/test/HelloWorld.java
+++ TeamProject/src/kr/co/test/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 public static void main(String[] args) {
 	System.out.println("Hello Git World");
 	System.out.println("second branch 에서 작업");
+	System.out.println("충돌 테스트를 위한 테스트");
 }
 }
